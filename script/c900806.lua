a5176ee967e216c78dd33ad24dbbb829c4796e00
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-04-15 13:52:54 +0100

    Create c900806.lua

---
+++ script/c900806.lua
@@ -0,0 +1,68 @@
+--Poltiquette Apparazor
+--Scripted by Beanbag
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_END_PHASE)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0,0x21,2000,0,4,RACE_FIEND,ATTRIBUTE_WIND) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
+		or not Duel.IsPlayerCanSpecialSummonMonster(tp,id,0,0x21,2000,0,4,RACE_FIEND,ATTRIBUTE_WIND) then return end
+	c:AddMonsterAttribute(TYPE_EFFECT+TYPE_TRAP)
+	Duel.SpecialSummonStep(c,1,tp,tp,true,false,POS_FACEUP)
+	c:AddMonsterAttributeComplete()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DIRECT_ATTACK)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BATTLE_START)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.condition)
+	e2:SetTarget(s.bttldestg)
+	e2:SetOperation(s.bttldesop)
+	c:RegisterEffect(e2)
+	Duel.SpecialSummonComplete()
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetAttacker()
+	local bc=Duel.GetAttackTarget()
+	if not bc then return false end
+	if tc:IsControler(1-tp) then tc,bc=bc,tc end
+	if tc:IsCode(id) and bc:IsSummonLocation(LOCATION_EXTRA) then
+		e:SetLabelObject(bc)
+		return true
+	else return false end
+end
+function s.bttldestg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	if chk==0 then return bc and bc:IsControler(1-tp) end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,Group.FromCards(bc,c),2,tp,0)
+end
+function s.bttldesop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	if c:IsRelateToBattle() and bc:IsRelateToBattle() then
+		Duel.Destroy(Group.FromCards(bc,c),REASON_EFFECT)
+	end
+end
