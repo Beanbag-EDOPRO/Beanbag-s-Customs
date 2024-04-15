555f8c57319d2eabd788f105c51416888a876945
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-04-15 10:34:42 +0100

    Update c900803.lua

--- script/c900803.lua
+++ script/c900803.lua
@@ -4,5 +4,9 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--fusion material
-	Fusion.AddProcMixRep(c,false,false,aux.FilterBoolFunctionEx(Card.IsSetCard,0x3D4),2,2)
+    Fusion.AddProcMix(c,true,true,s.ffilter,aux.FilterBoolFunctionEx(Card.IsType,TYPE_SPELL+TRAP),2,2)
+end
+
+function s.ffilter(c,fc,sumtype,tp)
+	return c:IsSetCard(0x3D4,fc,sumtype,tp) and c:IsType(TYPE_SPELL+TRAP,fc,sumtype,tp)
 end
