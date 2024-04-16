eee2d5c9896e12c40b6168eb88a1d74f38ae9d98
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-04-16 04:17:35 +0100

    Update c900803.lua

--- script/c900803.lua
+++ script/c900803.lua
@@ -5,8 +5,33 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--fusion material
 	Fusion.AddProcMixN(c,true,true,aux.FilterBoolFunctionEx(s.matfilter),2)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION) end)
+	e1:SetCost(aux.selfreleasecost)
+	e1:SetTarget(s.settg)
+	e1:SetOperation(s.setop)
+	c:RegisterEffect(e1)
 end
 
 function s.matfilter(c,fc,sumtype,tp)
 	return c:IsSpellTrap(fc,sumtype,tp) and c:IsSetCard(0x3D4,fc,sumtype,tp)
+end
+function s.setfilter(c)
+	return c:IsSetCard(0x3D4) and c:IsSpellTrap() and c:IsType(TYPE_CONTINUOUS) and c:IsSSetable()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>=2
+		and Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,2,nil) end
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<2 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,2,2,nil)
+	if #g>0 then
+		Duel.SSet(tp,g)
+	end
 end
