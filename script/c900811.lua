389dcd626e8110c4f041bc3c6a0a367a4c9823e3
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-04-17 10:55:21 +0100

    hue

--- script/c900811.lua
+++ script/c900811.lua
@@ -16,6 +16,73 @@ function s.initial_effect(c)
 	e2:SetTarget(s.thtg)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+	e3:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCost(s.actcost)
+	e3:SetOperation(s.actop)
+	c:RegisterEffect(e3)
+end
+
+function s.actfilter(c)
+	return c:IsSetCard(0x3D4) and c:IsTrap() and c:IsType(TYPE_CONTINUOUS) and c:CheckActivateEffect(false,false,false)~=nil
+end
+
+function s.actcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToGraveAsCost() and c:IsStatus(STATUS_EFFECT_ENABLED) end
+	Duel.SendtoGrave(c,REASON_COST)
+end
+function s.actop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local tc=Duel.SelectMatchingCard(tp,s.tffilter,tp,LOCATION_HAND,0,1,1,nil):GetFirst()
+	local tpe=tc:GetType()
+	local te=tc:GetActivateEffect()
+	local tg=te:GetTarget()
+	local co=te:GetCost()
+	local op=te:GetOperation()
+	e:SetCategory(te:GetCategory())
+	e:SetProperty(te:GetProperty())
+	Duel.ClearTargetCard()
+	local loc=LOCATION_SZONE
+	if (tpe&TYPE_FIELD)~=0 then
+		local fc=Duel.GetFieldCard(tp,LOCATION_FZONE,0)
+		if fc then Duel.SendtoGrave(fc,REASON_RULE) end
+		if Duel.GetFlagEffect(tp,62765383)>0 then
+			fc=Duel.GetFieldCard(1-tp,LOCATION_FZONE,0)
+			if fc and Duel.Destroy(fc,REASON_RULE)==0 then Duel.SendtoGrave(tc,REASON_RULE) end
+		end
+		loc=LOCATION_FZONE
+	end
+	Duel.MoveToField(tc,tp,tp,loc,POS_FACEUP,true)
+	Duel.Hint(HINT_CARD,0,tc:GetCode())
+	tc:CreateEffectRelation(te)
+	if (tpe&TYPE_EQUIP+TYPE_CONTINUOUS+TYPE_FIELD)==0 then
+		tc:CancelToGrave(false)
+	end
+	if co then co(te,tp,eg,ep,ev,re,r,rp,1) end
+	if tg then tg(te,tp,eg,ep,ev,re,r,rp,1) end
+	Duel.BreakEffect()
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	if g then
+		local etc=g:GetFirst()
+		while etc do
+			etc:CreateEffectRelation(te)
+			etc=g:GetNext()
+		end
+	end
+	if op then op(te,tp,eg,ep,ev,re,r,rp) end
+	tc:ReleaseEffectRelation(te)
+	if etc then
+		etc=g:GetFirst()
+		while etc do
+			etc:ReleaseEffectRelation(te)
+			etc=g:GetNext()
+		end
+	end
 end
 function s.tdfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0x3D4) and c:IsSpellTrap() and not c:IsCode(id)
