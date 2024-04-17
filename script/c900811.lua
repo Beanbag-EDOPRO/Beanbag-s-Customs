976c40a190171940de516350fbbe4050249131e9
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-04-17 19:25:03 +0100

    Update c900811.lua

--- script/c900811.lua
+++ script/c900811.lua
@@ -2,70 +2,104 @@
 --Scripted by Beanbag
 local s,id=GetID()
 function s.initial_effect(c)
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e1)
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_TODECK)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetCountLimit(1,id)
-	e2:SetTarget(s.thtg)
-	e2:SetOperation(s.thop)
-	c:RegisterEffect(e2)
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetCode(EFFECT_TRAP_ACT_IN_HAND)
-	e3:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetCountLimit(1,{id,1})
-	e3:SetCost(s.actcost)
-	e3:SetOperation(s.actop)
-	c:RegisterEffect(e3)
-end
-
-function s.actfilter(c)
-	return c:IsSetCard(0x3D4) and c:IsTrap() and c:IsType(TYPE_CONTINUOUS) and c:CheckActivateEffect(false,false,false)~=nil
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(e1)
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_TODECK)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_SZONE)
+    e2:SetCountLimit(1,{id,0})
+    e2:SetTarget(s.thtg)
+    e2:SetOperation(s.thop)
+    c:RegisterEffect(e2)
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_SZONE)
+    e3:SetCountLimit(1,{id,1})
+    e3:SetCost(s.actcost)
+    e3:SetTarget(s.acttg)
+    e3:SetOperation(s.actop)
+    c:RegisterEffect(e3)
 end
 
 function s.actcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:IsAbleToGraveAsCost() and c:IsStatus(STATUS_EFFECT_ENABLED) end
-	Duel.SendtoGrave(c,REASON_COST)
+    local c=e:GetHandler()
+    if chk==0 then return c:IsAbleToGraveAsCost() and c:IsStatus(STATUS_EFFECT_ENABLED) end
+    Duel.SendtoGrave(c,REASON_COST)
 end
-function s.actop(e,tp,eg,ep,ev,re,r,rp)
-local g=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_HAND,0,nil,tp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-		local sc=g:Select(tp,1,1,nil):GetFirst()
-		Duel.MoveToField(sc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
-		local te=sc:GetActivateEffect()
-		local tep=sc:GetControler()
-		local cost=te:GetCost()
-		if cost then cost(te,tep,eg,ep,ev,re,r,rp,1) end
-	Duel.RaiseEvent(sc,id,te,0,tp,tp,Duel.GetCurrentChain())
+
+--activate from the hand as new chain
+function s.actfilter(c,e,tp)
+    if not c:IsContinuousTrap() then return end
+    return c:IsSetCard(0x3D4) and c:CheckActivateEffect(false,false,false)~=nil and c:IsContinuousTrap()
 end
+
+function s.acttg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.IsExistingMatchingCard(s.actfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+end
+
+function s.acop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetMatchingGroup(s.actfilter,tp,LOCATION_HAND,0,nil,e,tp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+    local sc=g:Select(tp,1,1,nil):GetFirst()
+    if not sc then return end
+    --activate
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+    e1:SetCode(EVENT_CHAIN_END)
+    e1:SetCountLimit(1)
+    e1:SetLabelObject(sc)
+    e1:SetOperation(s.faop)
+    Duel.RegisterEffect(e1,tp)
+    sc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD+RESET_PHASE+PHASE_END,0,0)
+
+end
+function s.faop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=e:GetLabelObject()
+    if not tc then return end
+    local te=tc:GetActivateEffect()
+    local tep=tc:GetControler()
+    if not te then return end
+    local pre={Duel.GetPlayerEffect(tp,EFFECT_CANNOT_ACTIVATE)}
+    if pre[1] then
+        for i,eff in ipairs(pre) do
+            local prev=eff:GetValue()
+            if type(prev)~='function' or prev(eff,te,tep) then return end
+        end
+    end
+    if tc:GetFlagEffect(id)==0 then return false end
+    if te and te:GetCode()==EVENT_FREE_CHAIN and te:IsActivatable(tep) then
+        Duel.Activate(te)
+        Duel.BreakEffect()
+        tc:ResetFlagEffect(id)
+    end
+    e:Reset()
+end
+
 function s.tdfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x3D4) and c:IsSpellTrap() and not c:IsCode(id)
+    return c:IsFaceup() and c:IsSetCard(0x3D4) and c:IsSpellTrap() and not c:IsCode(id)
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and s.tdfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_REMOVED,0,1,nil)
-		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_REMOVED,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+    if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and s.tdfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_REMOVED,0,1,nil)
+        and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+    local g=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_REMOVED,0,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)~=0 then
-	Duel.ShuffleDeck(tp)
-	Duel.BreakEffect()
-	Duel.Draw(tp,1,REASON_EFFECT)
-	end
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)~=0 then
+    Duel.ShuffleDeck(tp)
+    Duel.BreakEffect()
+    Duel.Draw(tp,1,REASON_EFFECT)
+    end
 end
