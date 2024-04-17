4f9855576c1098f9855ecf016b12c8928b1180a8
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-04-17 07:36:17 +0100

    Update c900810.lua

--- script/c900810.lua
+++ script/c900810.lua
@@ -11,28 +11,28 @@ function s.initial_effect(c)
 	e2:SetCategory(CATEGORY_TOGRAVE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
     e2:SetRange(LOCATION_FZONE)
---[[	e2:SetCountLimit(1,id)]]
-	e2:SetTarget(s.thtg)
-	e2:SetOperation(s.thop)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.tgtg)
+	e2:SetOperation(s.tgop)
 	c:RegisterEffect(e2)
 end
 
-function s.thfilter(c)
-	return c:IsSpellTrap() and c:IsSetCard(0x3D4) and (c:IsAbleToGrave() or c:IsSSetable())
+function s.tgfilter(c)
+	return c:IsSetCard(0x3D4) and c:IsSpellTrap() and (c:IsAbleToGrave() or c:IsSSetable())
 end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
-	local tc=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
-	if not tc then return end
-	aux.ToGraveOrElse(tc,tp,
-		Card.IsSSetable,
-		function(c)
-			Duel.SSet(tp,tc)
-		end,
-		aux.Stringid(id,0)
-	)
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
+	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil)
+	if #g>0 then
+		local tc=g:GetFirst()
+		if tc and tc:IsAbleToGrave() and (not tc:IsSSetable() or Duel.SelectYesNo(tp,aux.Stringid(id,0))) then
+			Duel.SendtoGrave(tc,REASON_EFFECT)
+		else
+			Duel.SSet(tc,REASON_EFFECT)
+		end
+	end
 end
