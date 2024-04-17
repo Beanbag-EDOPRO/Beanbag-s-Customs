0de828a644451bc159624bc68d868fbd4d66f4e1
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-04-17 08:22:41 +0100

    Update c900810.lua

--- script/c900810.lua
+++ script/c900810.lua
@@ -39,9 +39,10 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		end,
 		aux.Stringid(id,0))
 	    local g=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,1))
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-		if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		if #g>0 then
 		Duel.BreakEffect()
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		Duel.ConfirmCards(1-tp,g)
