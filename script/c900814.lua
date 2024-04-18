cad04df45cf9dfe62938fea90e5c43d12a3a13b8
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-04-18 06:34:09 +0100

    Update c900814.lua

--- script/c900814.lua
+++ script/c900814.lua
@@ -5,8 +5,28 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--fusion material
 	Fusion.AddProcMixN(c,true,true,aux.FilterBoolFunctionEx(s.matfilter),3,99)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetOperation(s.sucop)
+	c:RegisterEffect(e2)
 end
 
 function s.matfilter(c,fc,sumtype,tp)
 	return c:IsSpellTrap(fc,sumtype,tp) and c:IsSetCard(0x3D4,fc,sumtype,tp)
+end
+
+function s.sucop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsSummonType(SUMMON_TYPE_FUSION) then return end
+	--Its original ATK/DEF become the number of Fusion Materials x 1000
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SET_BASE_ATTACK)
+	e1:SetValue(c:GetMaterialCount()*1000)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_SET_BASE_DEFENSE)
+	c:RegisterEffect(e2)
 end
