d7d2d12e8ac2ba033c8df06ac2f021f008e475cb
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-04-18 06:59:14 +0100

    Update c900814.lua

--- script/c900814.lua
+++ script/c900814.lua
@@ -4,12 +4,32 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--fusion material
-	Fusion.AddProcMixRep(c,true,true,s.matfilter,2,99)
+	Fusion.AddProcMixRep(c,true,true,s.matfilter,3,99)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetOperation(s.sucop)
+	c:RegisterEffect(e1)
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetOperation(s.sucop)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.negcon)
+	e2:SetTarget(s.negtg)
+	e2:SetOperation(s.negop)
 	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_IMMUNE_EFFECT)
+	e3:SetRange(LOCATION_ONFIELD)
+	e3:SetTargetRange(LOCATION_ONFIELD,0)
+	e3:SetTarget(s.indes)
+	e3:SetValue(s.unval)
+	c:RegisterEffect(e3)
 end
 
 function s.matfilter(c,fc,sumtype,tp)
@@ -30,3 +50,33 @@ function s.sucop(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetCode(EFFECT_SET_BASE_DEFENSE)
 	c:RegisterEffect(e2)
 end
+
+function s.indes(e,c)
+	return c:IsFaceup() and c:IsTrap() and c:IsType(TYPE_CONTINUOUS) and c:IsSetCard(0x3D4)
+end
+function s.unval(e,te)
+	return te:GetOwnerPlayer()~=e:GetHandlerPlayer() and te:IsActivated()
+end
+
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return rp==1-tp and Duel.IsChainNegatable(ev) and not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED)
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_REMOVE,nil,1,PLAYER_ALL,LOCATION_ONFIELD)
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.NegateActivation(ev) then return end
+	local c=e:GetHandler()
+	local g=Duel.GetFieldGroup(tp,LOCATION_ONFIELD,LOCATION_ONFIELD)
+		and #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local dg=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+		if #dg>0 then
+			Duel.HintSelection(dg,true)
+			Duel.BreakEffect()
+			Duel.Remove(dg,POS_FACEDOWN,REASON_EFFECT)
+		end
+	end
