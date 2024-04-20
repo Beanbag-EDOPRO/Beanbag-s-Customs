b0852390da5a9bceda0adddc9039daf9b0b91f60
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-04-20 15:13:32 +0100

    hue

---
+++ script/c900817.lua
@@ -0,0 +1,58 @@
+--Poltiquette Slumberstine
+--Scripted by Beanbag
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_FIEND),4,2,nil,nil,99)
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_MATERIAL_CHECK)
+	e0:SetValue(s.valcheck)
+	c:RegisterEffect(e0)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_EXTRA_FUSION_MATERIAL)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_RANGE)
+	e1:SetTarget(s.mttg)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTarget(s.desreptg)
+	e2:SetValue(s.desrepval)
+	e2:SetOperation(s.desrepop)
+	c:RegisterEffect(e2)
+end
+function s.matfilter(c)
+	return c:IsContinuousTrap() and c:IsAbleToGrave()
+end
+function s.valcheck(e,c)
+	local g=c:GetMaterial()
+	if g:IsExists(Card.IsSetCard,1,nil,0x3D4,s.matfilter) then
+		local reset=RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD+RESET_PHASE+PHASE_END
+		c:RegisterFlagEffect(id,reset,0,1)
+	end
+end
+function s.mttg(e,c)
+	return e:GetHandler():GetOverlayGroup():IsContains(c)
+end
+function s.repfilter(c,tp)
+	return c:IsControler(tp) and c:IsLocation(LOCATION_ONFIELD) and c:IsSetCard(0x3D4) and c:IsType(TYPE_FUSION)
+		and c:IsReason(REASON_EFFECT) and not c:IsReason(REASON_REPLACE)
+end
+function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return eg:IsExists(s.repfilter,1,nil,tp)
+		and c:CheckRemoveOverlayCard(tp,1,REASON_EFFECT) end
+	return Duel.SelectEffectYesNo(tp,c,96)
+end
+function s.desrepval(e,c)
+	return s.repfilter(c,e:GetHandlerPlayer())
+end
+function s.desrepop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_EFFECT)
+	Duel.Hint(HINT_CARD,0,id)
+end
