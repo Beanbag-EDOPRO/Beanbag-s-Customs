025ef2669bbee6aa4f0708d43c55d47412566eab
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-07-25 19:43:48 +0100

    hue

--- script/c900702.lua
+++ script/c900702.lua
@@ -4,62 +4,43 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--Synchro Summon
 	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x3D),2,2,Synchro.NonTunerEx(Card.IsSetCard,0x3D),1,99)
-		local e0=Effect.CreateEffect(c)
-	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e0:SetValue(aux.synlimit)
-	c:RegisterEffect(e0)
-	--Special Summon procedure
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetRange(LOCATION_EXTRA)
-	e1:SetCondition(s.spcon)
-	e1:SetTarget(s.sptg)
-	e1:SetOperation(s.spop)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetLocation(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
 end
-function s.spfilter1(c,tp)
-	return c:IsSetCard(0x3D) and c:IsType(TYPE_TUNER) and c:IsFaceup()
-		and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true,true)
+function s.cfilter(c)
+	return (c:IsFaceup() or c:IsLocation(LOCATION_GRAVE)) and c:IsSetCard(0x3D) and c:IsAbleToRemoveAsCost()
 end
-function s.spfilter2(c,tp)
-	return c:IsCode(CARD_GREAT_SHOGUN_SHIEN) and c:IsFaceup()
-		and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true,true)
+function s.filter(c)
+	return c:IsSetCard(0x3D) and c:IsType(TYPE_TUNER) and c:IsSummonable(true,nil)
 end
-function s.rescon(sg,e,tp)
-	return Duel.GetLocationCountFromEx(tp,tp,sg,e:GetHandler())>0
-		and sg:FilterCount(s.spfilter1,nil,tp)==1
-		and sg:FilterCount(s.spfilter2,nil,tp)==1
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler())
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	local dg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil)
+	local ct=math.min(2,dg:GetClassCount(Card.GetCode))
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local rg=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,1,ct,e:GetHandler())
+	local rc=Duel.Remove(rg,POS_FACEUP,REASON_COST)
+	Duel.SetTargetParam(rc)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,rc,tp,LOCATION_DECK)
 end
-function s.spcon(e,c)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	local g1=Duel.GetMatchingGroup(s.spfilter1,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil,tp)
-	local g2=Duel.GetMatchingGroup(s.spfilter2,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,nil,tp)
-	local g=g1:Clone()
-	g:Merge(g2)
-	return #g1>0 and #g2>0 and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0)
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-	local g1=Duel.GetMatchingGroup(s.spfilter1,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil,tp)
-	local g2=Duel.GetMatchingGroup(s.spfilter2,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil,tp)
-	local rg=g1:Clone()
-	rg:Merge(g2)
-	local g=aux.SelectUnselectGroup(rg,e,tp,2,2,s.rescon,1,tp,HINTMSG_REMOVE,nil,nil,true)
-	if #g>0 then
-		g:KeepAlive()
-		e:SetLabelObject(g)
-		return true
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local dg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil)
+	local ct=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+	if dg:GetClassCount(Card.GetCode)==0 or dg:GetClassCount(Card.GetCode)<ct then return end
+	local g=Group.CreateGroup()
+	for i=1,ct do
+		local tc=dg:Select(tp,1,1,nil):GetFirst()
+		g:AddCard(tc)
+		dg:Remove(Card.IsCode,nil,tc:GetCode())
 	end
-	return false
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=e:GetLabelObject()
-	if not g then return end
-	Duel.Remove(g,POS_FACEUP,REASON_COST)
-	g:DeleteGroup()
+	Duel.SpecialSummon(g,nil,REASON_EFFECT)
 end
