e851453ee141d24cd7ed3991ae3924c2d26b1ffe
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-07-25 18:46:34 +0100

    Create c900702.lua

---
+++ script/c900702.lua
@@ -0,0 +1,6 @@
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Synchro Summon
+	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x3D),2,2,Synchro.NonTunerEx(Card.IsSetCard,0x3D),1,99)
+end
