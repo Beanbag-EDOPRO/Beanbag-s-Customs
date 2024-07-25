83d9e7e28cbbdf09ab7693cca5799577762d2226
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-07-25 19:20:37 +0100

    Update c900702.lua

--- script/c900702.lua
+++ script/c900702.lua
@@ -19,20 +19,13 @@ function s.spfilter(c,tp)
 	return c:IsCode(63176202) and c:IsFaceup()
 		and c:IsAbleToRemoveAsCost()
 end
-function s.rescon(sg,e,tp)
-	return Duel.GetLocationCountFromEx(tp,tp,sg,e:GetHandler())>0
-		and sg:FilterCount(s.spfilter,nil,tp)==1
-end
 function s.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	local g1=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil,tp)
-	return #g1>0 and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
 	local g1=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil,tp)
-	
-	local g=aux.SelectUnselectGroup(rg,e,tp,2,2,s.rescon,1,tp,HINTMSG_REMOVE,nil,nil,true)
 	if #g>0 then
 		g:KeepAlive()
 		e:SetLabelObject(g)
