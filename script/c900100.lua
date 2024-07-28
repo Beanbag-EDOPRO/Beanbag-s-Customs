813abc9dc95010c1576187bb53f26c872ad9fdb4
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-07-28 15:55:58 +0100

    Update c900100.lua

--- script/c900100.lua
+++ script/c900100.lua
@@ -131,7 +131,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetCode(EFFECT_DISABLE_EFFECT)
 		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e2,true)
-	Duel.SpecialSummon(tc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)>0 then
+	Duel.SpecialSummon(tc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)>0
 		local c=e:GetHandler()
 		if c:IsRelateToEffect(e) then
 			Duel.Overlay(tc,c)
