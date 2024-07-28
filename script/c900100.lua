ab46120f4103873a231a1a4c0d89b8659a18435f
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-07-28 13:29:53 +0100

    lol

--- script/c900100.lua
+++ script/c900100.lua
@@ -19,11 +19,11 @@ end
 function s.selfspcon(e,c)
 	if c==nil then return true end
 	local tp=e:GetHandlerPlayer()
-	local g=Duel.GetMatchingGroup(s.selfspconfilter,tp,LOCATION_EXTRA,0,nil)
+	local g=Duel.GetMatchingGroup(s.selfspconfilter,tp,LOCATION_EXTRA+LOCATION_ONFIELD,0,nil)
 	return #g>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 end
 function s.selfsptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
-	local rg=Duel.GetMatchingGroup(s.selfspconfilter,tp,LOCATION_EXTRA,0,nil)
+	local rg=Duel.GetMatchingGroup(s.selfspconfilter,tp,LOCATION_EXTRA+LOCATION_ONFIELD,0,nil)
 	local g=aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),1,tp,HINTMSG_TOGRAVE,nil,nil,true)
 	if #g>0 then
 		g:KeepAlive()
@@ -35,5 +35,5 @@ end
 function s.selfspop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=e:GetLabelObject()
 	if not g then return end
-		Duel.Release(g,REASON_COST)
+	Duel.SendtoGrave(g,REASON_COST)
 end
