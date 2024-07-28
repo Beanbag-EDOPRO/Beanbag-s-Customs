efa1e669df649f8b92cd257b810fcfc73c2ee32c
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-07-28 13:12:55 +0100

    Update c900100.lua

--- script/c900100.lua
+++ script/c900100.lua
@@ -1,5 +1,6 @@
-	local s,id=GetID()
+local s,id=GetID()
 function s.initial_effect(c)
+	--Special Summon this card from the hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -7,31 +8,32 @@ function s.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-	e1:SetCondition(s.spcon)
-	e1:SetTarget(s.sptg)
-	e1:SetOperation(s.spop)
+	e1:SetCondition(s.selfspcon)
+	e1:SetTarget(s.selfsptg)
+	e1:SetOperation(s.selfspop)
 	c:RegisterEffect(e1)
 end
-function s.cfilter(c)
-	return c:IsType(TYPE_XYZ) and c:IsSetCard(0x385)
+function s.selfspconfilter(c)
+	return c:IsType(TYPE_XYZ) and c:IsSetCard(0x385) and c:IsAbleToGraveAsCost()
 end
-function s.spcon(e,c)
+function s.selfspcon(e,c)
 	if c==nil then return true end
-	local tp=c:GetControler()
-	return Duel.CheckReleaseGroup(s.cfilter,tp,LOCATION_EXTRA+ONFIELD,0,nil)
+	local tp=e:GetHandlerPlayer()
+	local g=Duel.GetMatchingGroup(s.selfspconfilter,tp,LOCATION_EXTRA,0,nil)
+	return #g>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
-	local g=Duel.SelectReleaseGroup(s.cfilter,tp,LOCATION_EXTRA+ONFIELD,0,nil)
-	if g then
+function s.selfsptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	local rg=Duel.GetMatchingGroup(s.selfspconfilter,tp,LOCATION_EXTRA,0,nil)
+	local g=aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),1,tp,HINTMSG_TOGRAVE,nil,nil,true)
+	if #g>0 then
 		g:KeepAlive()
 		e:SetLabelObject(g)
 		return true
 	end
 	return false
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+function s.selfspop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=e:GetLabelObject()
 	if not g then return end
-	Duel.Release(g,REASON_COST)
-	g:DeleteGroup()
+		Duel.Release(g,REASON_COST)
 end
