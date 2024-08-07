8fe48a37749350a8ad8a80d6ada43198b4d892df
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-08-07 11:35:31 +0100

    Update c900113.lua

--- script/c900113.lua
+++ script/c900113.lua
@@ -7,7 +7,7 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCountLimit(1,id)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 	local e2=Effect.CreateEffect(c)
@@ -18,7 +18,20 @@ function s.initial_effect(c)
 	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x385))
 	e2:SetValue(500)
 	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_RELEASE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCost(s.tribcost)
+	e3:SetCondition(s.tribcon)
+	e3:SetTarget(s.tribtg)
+	e3:SetOperation(s.tribop)
+	c:RegisterEffect(e3)
 end
+
 function s.thfilter(c)
 	return c:IsSetCard(0x385) and c:IsSpellTrap() and c:IsAbleToHand() and not c:IsCode(id)
 end
@@ -30,5 +43,27 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		local sg=g:Select(tp,1,1,nil)
 		Duel.SendtoHand(sg,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,sg)
+	end
+end
+function s.tribcon(e,tp,eg,ep,ev,re,r,rp)
+	return c:IsSummonType(SUMMON_TYPE_XYZ) and c:IsSetCard(0x385)
+end
+function s.tribfilter(c,tp)
+	return Duel.CheckReleaseGroup(1-tp,nil,1,c)
+end
+function s.tribcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.tribfilter,1,false,nil,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local g=Duel.SelectReleaseGroupCost(tp,s.tribfilter,1,1,false,nil,nil,tp)
+	Duel.Release(g,REASON_COST)
+end
+function s.tribtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(1-tp,nil,1,nil) end
+end
+function s.tribop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_RELEASE)
+	local g=Duel.SelectReleaseGroup(1-tp,nil,1,1,nil)
+	if g then
+		Duel.Release(g,REASON_RULE,1-tp)
 	end
 end
