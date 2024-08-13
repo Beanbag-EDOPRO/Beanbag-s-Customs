56d49a3739bb822b3311837d084ac39770ef5a06
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-08-13 11:39:38 +0100

    Update c900113.lua

--- script/c900113.lua
+++ script/c900113.lua
@@ -20,14 +20,14 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_RELEASE)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCategory(CATEGORY_DRAW)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e3:SetRange(LOCATION_FZONE)
-	e3:SetCountLimit(1,{id,1})
-	e3:SetCondition(s.tribcon)
-	e3:SetTarget(s.tribtg)
-	e3:SetOperation(s.tribop)
+	e3:SetCost(s.cost)
+	e3:SetCondition(s.thcon)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
 	c:RegisterEffect(e3)
 end
 
@@ -44,28 +44,28 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ConfirmCards(1-tp,sg)
 	end
 end
-function s.tribfilter2(c)
-	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsSetCard(0x385)
+function s.sfilter(c)
+	return c:IsSummonType(SUMMON_TYPE_XYZ)
 end
-function s.tribcon(e,tp,eg,ep,ev,re,r,rp)
-    return eg:IsExists(s.tribfilter2,1,nil)
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.sfilter,1,nil)
 end
-
-function s.tribtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and Duel.IsPlayerCanDraw(1-tp,1) end
-	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,PLAYER_ALL,1)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,PLAYER_ALL,1)
+function s.cfilter(c,tp)
+	return Duel.CheckReleaseGroup(1-tp,nil,1,c)
 end
-function s.tribop(e,tp,eg,ep,ev,re,r,rp)
-	local h1=Duel.Draw(tp,1,REASON_EFFECT)
-	local h2=Duel.Draw(1-tp,1,REASON_EFFECT)
-	if h1>0 or h2>0 then Duel.BreakEffect() end
-	if h1>0 then
-		Duel.ShuffleHand(tp)
-		Duel.DiscardHand(tp,aux.TRUE,1,1,REASON_EFFECT+RELEASE)
-	end
-	if h2>0 then 
-		Duel.ShuffleHand(1-tp)
-		Duel.DiscardHand(1-tp,aux.TRUE,1,1,REASON_EFFECT+REASON_RELEASE)
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,nil,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local g=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,nil,nil,tp)
+	Duel.Release(g,REASON_COST)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(1-tp,nil,1,nil) end
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_RELEASE)
+	local g=Duel.SelectReleaseGroup(1-tp,nil,1,1,nil)
+	if g then
+		Duel.Release(g,REASON_RULE,1-tp)
 	end
 end
