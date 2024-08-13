1ced5f0fad5a190d386e0d7ca184cdc14c836156
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-08-13 15:22:02 +0100

    Create c900111.lua

---
+++ script/c900111.lua
@@ -0,0 +1,35 @@
+--Magmalith Eruption
+--scripted by Beanbag
+local s,id=GetID()
+function s.initial_effect(c)
+    local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	e1:SetCondition(s.con)
+	c:RegisterEffect(e1)
+end
+function s.filter1(c)
+	return c:IsFaceup() and c:IsSetCard(0x385)
+end
+function s.filter1(c)
+	return c:IsFaceup() and c:IsCode(900110)
+end
+function s.con(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+		and	Duel.IsExistingMatchingCard(s.filter1,c:GetControler(),LOCATION_MZONE,0,2,nil)
+		and or Duel.IsExistingMatchingCard(s.filter2,c:GetControler(),LOCATION_MZONE,0,1,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler()) end
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_RELEASE,g,#g,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())
+	Duel.Release(g,REASON_EFFECT)
+end
