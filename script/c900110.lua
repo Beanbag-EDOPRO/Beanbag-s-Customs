cc3179e3344ebee06c0d0099c7bb73522e8073f7
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-08-13 12:34:30 +0100

    Update c900110.lua

--- script/c900110.lua
+++ script/c900110.lua
@@ -2,12 +2,19 @@
 --scripted by Beanbag
 local s,id=GetID()
 function s.initial_effect(c)
-	--Must be properly summoned before reviving
 	c:EnableReviveLimit()
-	--Fusion summon/contact fusion procedure
 	Fusion.AddProcMixN(c,true,true,aux.FilterBoolFunctionEx(s.matfilter),2,aux.FilterBoolFunctionEx(s.matfilter2),1)
 	Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit)
+	local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.cost)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
 end
+
+--MATERIALS
 
 function s.matfilter(c,fc,st,tp)
 	return c:IsSetCard(0x385) and not c:IsType(TYPE_XYZ)
@@ -23,4 +30,34 @@ function s.contactop(g)
 end
 function s.splimit(e,se,sp,st)
 	return e:GetHandler():GetLocation()~=LOCATION_EXTRA
+end
+
+--TRIBUTE EFFECT
+
+function s.thcfilter(c,tp)
+	return c:IsMonster() and c:IsSetCard(0x385) and c:IsReleasable()
+end
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.thcfilter,2,true,nil,nil,tp) end
+	local g=Duel.SelectReleaseGroupCost(tp,s.thcfilter,1,1,true,nil,nil,tp)
+	Duel.Release(g,REASON_COST)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e1:SetTargetRange(LOCATION_ONFIELD,0)
+	e1:SetTarget(s.tgfilter)
+	e1:SetValue(1)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetValue(aux.tgoval)
+	Duel.RegisterEffect(e2,tp)
+end
+function s.tgfilter(e,c)
+	return c:IsFaceup() and c:IsSetCard(0x385)
 end
