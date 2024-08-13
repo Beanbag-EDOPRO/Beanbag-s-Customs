996d9ab3e7ea1f34b114b2731f64af453c02c2de
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-08-13 13:10:43 +0100

    Create c900115.lua

---
+++ script/c900115.lua
@@ -0,0 +1,38 @@
+--Magmalith Brimstone
+--scripted by Beanbag
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.sendcost)
+	e1:SetTarget(s.sendtarget)
+	e1:SetOperation(s.sendactivate)
+	c:RegisterEffect(e1)
+end
+function s.sendfilter(c,tp)
+	return c:IsSetCard(0x385) and c:IsMonster() and c:IsReleasable()
+end
+function s.sendcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.sendfilter,1,true,nil,nil,tp) end
+	local g=Duel.SelectReleaseGroupCost(tp,s.sendfilter,1,1,true,nil,nil,tp)
+	Duel.Release(g,REASON_COST)
+end
+function s.sendfilter2(c)
+	return c:IsSetCard(0x385) and c:IsMonster() and c:IsAbleToGrave()
+end
+function s.sendtarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.sendfilter2,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK+LOCATION_EXTRA)
+end
+function s.sendactivate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.sendfilter2,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoGrave(g,REASON_EFFECT)
+	end
+end
