85e5dd53bf32c63d4d44096ba75c9dc38ee6cc05
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-08-28 13:17:37 +0100

    update

--- script/c900110.lua
+++ script/c900110.lua
@@ -15,6 +15,15 @@ function s.initial_effect(c)
 	e1:SetCost(s.cost)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_RELEASE)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e3:SetCountLimit(1,{id,0})
+	e3:SetTarget(s.addtg)
+	e3:SetOperation(s.addop)
+	c:RegisterEffect(e3)
 end
 
 --MATERIALS
@@ -63,4 +72,27 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.tgfilter(e,c)
 	return c:IsFaceup() and c:IsSetCard(0x385)
+end
+
+--TRIBUTED EFFECT
+
+function s.addfilter(c)
+	return c:IsSetCard(0x385) and c:IsAbleToHand() and not c:IsCode(id)
+end
+function s.addtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.addfilter,tp,LOCATION_GRAVE,0,2,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,s.addfilter,tp,LOCATION_GRAVE,0,2,2,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,0,0)
+end
+function s.addop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetTargetCards(e)
+	if Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
+		Duel.ConfirmCards(1-tp,g)
+		if c:IsFaceup() and c:IsRelateToEffect(e) then
+			Duel.RaiseSingleEvent(c,EVENT_CUSTOM+id,re,r,rp,0,0)
+		end
+	end
 end
