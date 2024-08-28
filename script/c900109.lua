b55f7627e4c1e72a59eba9e51c1a955fdd4c2095
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-08-28 16:00:37 +0100

    Update c900109.lua

--- script/c900109.lua
+++ script/c900109.lua
@@ -52,17 +52,14 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	local g=eg:Filter(Card.IsControler,nil,1-tp)
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE_SUMMON,g,#g,0,0)
-	local g2=g:Filter(Card.IsPreviousLocation,nil,LOCATION_EXTRA)
-	if #g2>0 then
-		Duel.SetOperationInfo(0,CATEGORY_TOEXTRA,g2,#g2,0,0)
-	end
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=eg:Filter(Card.IsSummonPlayer,nil,1-tp)
+	local g=eg:Filter(Card.IsControler,nil,1-tp)
 	Duel.NegateSummon(g)
-	Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+	Duel.Destroy(g,REASON_EFFECT)
 end
+
 
 --IMMUNITY
 
