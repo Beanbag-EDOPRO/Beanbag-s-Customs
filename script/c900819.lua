47643105e56996fbc10b4502db46b0d881e75ca3
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2024-09-07 17:58:26 +0100

    Update c900819.lua

--- script/c900819.lua
+++ script/c900819.lua
@@ -123,19 +123,11 @@ end
 
 function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 local bc=Duel.GetFirstTarget()
-if bc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0
+if bc:IsRelateToEffect(e) and Duel.SpecialSummon(bc,0,tp,tp,false,false,POS_FACEUP)>0
 		and c:IsRelateToEffect(e) and c:IsControler(tp) and Duel.Equip(tp,c,tc) then
 		c:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,1))
-		--The equipped monster gains ATK equal to total Link Rating of Link Monsters equipped to it x 600
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_EQUIP)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(s.atkval)
-		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
-		c:RegisterEffect(e1)
     end
 end
-
 
 
 
