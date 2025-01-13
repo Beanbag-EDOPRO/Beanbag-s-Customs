e660d93fb40d7656b416da46c3cd72edc3ff2aff
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2025-01-13 17:05:21 +0000

    hue

--- script/c999000.lua
+++ script/c999000.lua
@@ -18,21 +18,21 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_RITUAL)
 end
 function s.filter(c)
-	return c:IsAbleToHand() and c:IsSetCard(0x270F)
+	return c:IsAbleToHand() and c:IsSetCard(0x270F) and not c:IsCode(999000)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
-	local ct=c:GetMaterialCount()
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil) end
+	local c=e:GetHandler()
+	local ct=c:IsRitualSummoned() and c:GetMaterialCount() or 0
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	Duel.SelectTarget(s.filter,tp,LOCATION_GRAVE,0,1,ct,c)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_GRAVE,0,nil)
-	local sg=aux.SelectUnselectGroup(g,e,tp,1,2,aux.dncheck,1,tp,HINTMSG_ATOHAND)
-	if #sg>0 then
+Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local ct=e:GetHandler():GetMaterialCount()
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_ONFIELD,0,1,nil)
+	if #g==0 or ct==0 then return false end
 		Duel.SendtoHand(sg,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,sg)
-end
 end
