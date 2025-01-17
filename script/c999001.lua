5016838c05eb18a23526b1a4d1a49b895370cf0d
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2025-01-17 09:32:49 +0000

    Update c999001.lua

--- script/c999001.lua
+++ script/c999001.lua
@@ -1,41 +1,120 @@
 --Holysphere Congregation
---Scripted by Beanbag
+--Scripted by Beanbag (Aimer was here)
 
 local s,id=GetID()
 function s.initial_effect(c)
-	local e1=Ritual.CreateProc({handler=c,lvtype=RITPROC_GREATER,filter=aux.FilterBoolFunction(Card.IsSetCard,0x270F),extrafil=s.extrafil,location=LOCATION_HAND|LOCATION_GRAVE})
-	c:RegisterEffect(e1)
+	--Activate
+	Ritual.AddProcGreater{handler=c,filter=s.ritualfil,matfilter=s.matfilter,extrafil=s.extragroup,extraop=s.extraop,forcedselection=s.tributelimit,stage2=s.stage2}
+	--Return to hand
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
     e2:SetCategory(CATEGORY_TOHAND)
     e2:SetType(EFFECT_TYPE_IGNITION)
     e2:SetRange(LOCATION_GRAVE)
     e2:SetCountLimit(1,id)	
-    e2:SetTarget(s.tg)
-	e2:SetOperation(s.op)
+    e2:SetTarget(s.rthtg)
+	e2:SetOperation(s.rthop)
 	c:RegisterEffect(e2)
 end
+
 s.listed_series={0x270F}
 s.listed_names={999000}
-function s.extrafilter(c)
-	return c:IsSetCard(0x270F) and c:HasLevel() and c:IsAbleToGrave()
+
+--Material Filters
+function s.ritualfil(c)
+    return c:IsSetCard(0x270F) and c:IsRitualMonster()
 end
-function s.extrafil(e,tp,eg,ep,ev,re,r,rp,chk)
-	return Duel.GetMatchingGroup(s.extrafilter,tp,LOCATION_DECK,0,nil)
+function s.matfilter(c)
+    return c:IsReleasableByEffect() and c:IsMonster() and c:IsCanBeRitualMaterial() and ((c:IsSetCard(0x270F) and c:IsLocation(LOCATION_DECK)) or (c:IsLocation(LOCATION_HAND|LOCATION_MZONE)))
 end
-function s.filter(c)
+function s.deckmatfilter(c)
+    return c:IsSetCard(0x270F) and c:IsReleasableByEffect() and c:IsCanBeRitualMaterial()
+end
+
+--Return Handlimit is above 0/Different name check function
+function s.extragroup(e,tp,eg,ep,ev,re,r,rp,chk)
+    local handlim=6
+    local hls=Duel.GetPlayerEffect(tp,EFFECT_HAND_LIMIT)
+    if hls then
+        handlim=hls:GetValue()
+    end
+    if handlim<=0 then return end
+    local g=Duel.GetMatchingGroup(s.deckmatfilter,tp,LOCATION_DECK,0,nil)
+    local newgroup=Group.CreateGroup()
+    local nametable={}
+    for tc in aux.Next(g) do
+        local name=tc:GetCode()
+        if not nametable[name] then
+            nametable[name]=true
+            newgroup:AddCard(tc)
+        end
+    end
+    return newgroup
+end
+
+--Release Extra Group from Deck
+function s.extraop(mat,e,tp,eg,ep,ev,re,r,rp,tc)
+    local mat2=mat:Filter(Card.IsLocation,nil,LOCATION_DECK)
+    mat:Sub(mat2)
+    Duel.ReleaseRitualMaterial(mat)
+    Duel.SendtoGrave(mat2,REASON_EFFECT|REASON_MATERIAL|REASON_RITUAL|REASON_RELEASE)
+end
+function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+
+
+--Force Allow Deck Materials up to your Handlimit Size
+function s.tributelimit(e,tp,g,sc)
+	-- Get the hand limit or set it to 6 by default
+    local handlim=6
+    local hls=Duel.GetPlayerEffect(tp,EFFECT_HAND_LIMIT)
+    if hls then
+        handlim=hls:GetValue()
+    end
+    local deckmat=g:Filter(Card.IsLocation,nil,LOCATION_DECK)
+	return #deckmat<=handlim,#deckmat>handlim
+end
+
+--Lose LP equal to # Sent from Deck*500
+function s.stage2(mg,e,tp,eg,ep,ev,re,r,rp,sc)
+    -- Get the hand limit or set it to 6 by default
+    local c=e:GetHandler()
+    local handlim=6
+    local hls=Duel.GetPlayerEffect(tp,EFFECT_HAND_LIMIT)
+    if hls then
+        handlim=hls:GetValue()
+    end
+    local ct=mg:FilterCount(Card.IsPreviousLocation,nil,LOCATION_DECK)
+    if ct>0 then 
+        Duel.SetLP(tp,Duel.GetLP(tp)-(ct*500))
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_FIELD)
+        e1:SetCode(EFFECT_HAND_LIMIT)
+        e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+        e1:SetTargetRange(1,0)
+        e1:SetValue(handlim-ct)
+        e1:SetReset(RESET_PHASE+PHASE_END)
+        Duel.RegisterEffect(e1,tp)
+    end
+end
+
+
+--Return to hand
+function s.rthfilter(c)
 	return c:IsSetCard(0x270F) and c:IsMonster() and c:IsAbleToDeck()
 end
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc) end
-	if chk==0 then return e:GetHandler():IsAbleToHand()
-		and Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil) end
+function s.rthtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.rthfilter(chkc) end
+	if chk==0 then return c:IsAbleToHand() and Duel.IsExistingTarget(s.rthfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,3,3,nil)
+	local g=Duel.SelectTarget(tp,s.rthfilter,tp,LOCATION_GRAVE,0,3,3,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,c,1,0,0)
 end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
+function s.rthop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	local c=e:GetHandler()
 	if tc and tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)~=0
