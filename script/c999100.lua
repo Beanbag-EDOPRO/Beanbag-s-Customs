93de22b8083e19dd3c157c5dc099c5c1074f5065
Beanbag-EDOPRO <160979770+Beanbag-EDOPRO@users.noreply.github.com>
2025-03-22 15:36:46 +0000

    Update c999100.lua

--- script/c999100.lua
+++ script/c999100.lua
@@ -5,40 +5,46 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--Cannot Special Summon
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
-	c:RegisterEffect(e0)
-	--Special Summon itself from the hand or GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetRange(LOCATION_HAND|LOCATION_GRAVE)
-	e1:SetCondition(s.spproccon)
-	e1:SetTarget(s.spproctg)
-	e1:SetOperation(s.spprocop)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
+	--Special Summon itself from the hand or GY
 end
 
-	function s.tdfilter(c)
-	return c:IsSetCard(0x238C) and c:IsMonster() and c:IsAbleToDeckAsCost() and not c:IsCode(999100)
+function s.spconfilter(c)
+	return c:IsSetCard(0x238C) and c:IsMonster() and c:IsAbleToDeckOrExtraAsCost() and not c:IsCode(999100)
 end
-function s.spproccon(e,c)
+function s.spcon(e,c)
 	if c==nil then return true end
 	local tp=e:GetHandlerPlayer()
-	local rg=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_HAND|LOCATION_GRAVE,0,e:GetHandler())
-	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and aux.SelectUnselectGroup(rg,e,tp,1,1,aux.dncheck,0)
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spconfilter,tp,LOCATION_HAND|LOCATION_GRAVE,0,1,c)
 end
-function s.spproctg(e,tp,eg,ep,ev,re,r,rp,c)
-	local rg=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_HAND|LOCATION_GRAVE,0,e:GetHandler())
-	local g=aux.SelectUnselectGroup(rg,e,tp,1,1,aux.dncheck,1,tp,HINTMSG_TODECK,nil,nil,true)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	local rg=Duel.GetMatchingGroup(s.spconfilter,tp,LOCATION_HAND|LOCATION_GRAVE,0,c)
+	local g=aux.SelectUnselectGroup(rg,e,tp,1,1,nil,1,tp,HINTMSG_TODECK,nil,nil,true)
 	if #g>0 then
 		g:KeepAlive()
 		e:SetLabelObject(g)
 		return true
 	end
 	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	if g:IsExists(Card.IsLocation,1,nil,LOCATION_HAND) then
+		Duel.ConfirmCards(1-tp,g)
+	else
+		Duel.HintSelection(g)
+	end
+	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)
+	g:DeleteGroup()
 end
